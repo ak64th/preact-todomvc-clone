d6fcb11d838134a697fcc17a1bc7719838f4581d
郁骏 <ak64th@gmail.com>
2018-05-21 13:09:09 +0800

    fix(store): fix for prerender

--- src/store.js
+++ src/store.js
@@ -1,8 +1,14 @@
 import { createStore } from 'redux';
 import reducer from './reducers';
 
-const store = createStore(
-  reducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-);
+let store;
+if (process.env.NODE_ENV === 'development') {
+  store = createStore(
+    reducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  );
+} else {
+  store = createStore(reducer);
+}
+
 export default store;
